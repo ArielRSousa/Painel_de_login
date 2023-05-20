4a16d1dcb4375078e6dcf60bb51790b312daf4df
ArielRSousa <contatoarieldzn@gmail.com>
2023-05-20 17:59:56 -0300

    Falha Mostrar senha e atualizações
    Nessa atualização concertei a falha que dava quando eu entrava no login ou errava a senha não funcionava a opção de mostrar a senha e oculta, e também adicionei um novo painel que e acionado quando eu faço o login

--- main.py
+++ main.py
@@ -1,41 +1,65 @@
 import customtkinter
+from tkinter import messagebox
 
+# Configurações
 customtkinter.set_appearance_mode('dark')
 customtkinter.set_default_color_theme("dark-blue")
 
+
+# Cria a janela
 janela = customtkinter.CTk()
-janela.geometry('500x300')
+janela.geometry('500x300')  
 janela.minsize(500, 300)
 janela.maxsize(500,300)
+janela.title("Projeto de Login")
 
+
+# Funções
 def clique():
-    print("Login feito com sucesso! [💚]")
-
+    global email, senha
+    if email.get() == "admin" and senha.get() == "admin":
+            fg_color = "green"
+            messagebox.showinfo("Login", "Login efetuado com sucesso!")
+            janela.destroy()
+            criar_nova_janela()
+    else:
+        messagebox.showerror("Login", "Usuario ou senha incorretos!", icon="warning")
+            
 
 def ocultar_senha():
-    global senha, botao_mostrar_senha
     senha.configure(show="*")
-    botao_mostrar_senha.configure(text='MOSTRAR SENHA', command=mostar_senha)
+    botao_mostrar_senha.configure(text='MOSTRAR SENHA', command=mostrar_senha)
 
-
-def mostar_senha():
-    global senha, botao_mostrar_senha
+def mostrar_senha():
     senha.configure(show="")
     botao_mostrar_senha.configure(text='OCULTAR SENHA', command=ocultar_senha)
 
 
+def criar_nova_janela():
+    janela = customtkinter.CTk()
+    janela.geometry('500x300')
+    janela.minsize(500, 300)
+    janela.maxsize(500, 300)
+    janela.title("Projeto de Login")
+    texto = customtkinter.CTkLabel(janela, text="Login efetuado com sucesso!", font=("Arial", 20))
+    texto.pack(padx=10, pady=10)
+    janela.mainloop()
+    
+
+# Widgets
 texto = customtkinter.CTkLabel(janela, text="Fazer Login")
-email = customtkinter.CTkEntry(janela, placeholder_text="E-mail")
+email = customtkinter.CTkEntry(janela, placeholder_text="Usuario")
 senha = customtkinter.CTkEntry(janela, placeholder_text="Senha", show='*')
-botao_mostrar_senha = customtkinter.CTkButton(janela, text="MOSTRAR A SENHA", command=mostar_senha)
+botao_mostrar_senha = customtkinter.CTkButton(janela, text="MOSTRAR A SENHA", command=mostrar_senha)
 botao_login = customtkinter.CTkButton(janela, text="Login", command=clique)
 
+# Posicionamento
 texto.pack(padx=10, pady=10)
 email.pack(padx=10, pady=10)
-senha.pack(padx=10, pady=10)
+senha.pack(padx=10, pady=10)    
 botao_login.pack(padx=60, pady=10)
 botao_mostrar_senha.pack(padx=10, pady=10)
 
 
-janela.mainloop()
- 
+# Inicia a janela
+janela.mainloop() 
