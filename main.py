febe91fff854c9217a9608ed8f49e37e55125037
ArielRSousa <contatoarieldzn@gmail.com>
2023-05-20 02:16:08 -0300

    Função Nova(Mostrar senha)
    Nessa atualização eu adiciono 2 funções novas que tem como objetivo mostrar e ocultar a senha digitada

--- main.py
+++ main.py
@@ -9,16 +9,33 @@ janela.minsize(500, 300)
 janela.maxsize(500,300)
 
 def clique():
-    print("Fazer Login")
+    print("Login feito com sucesso! [💚]")
+
+
+def ocultar_senha():
+    global senha, botao_mostrar_senha
+    senha.configure(show="*")
+    botao_mostrar_senha.configure(text='MOSTRAR SENHA', command=mostar_senha)
+
+
+def mostar_senha():
+    global senha, botao_mostrar_senha
+    senha.configure(show="")
+    botao_mostrar_senha.configure(text='OCULTAR SENHA', command=ocultar_senha)
+
 
 texto = customtkinter.CTkLabel(janela, text="Fazer Login")
 email = customtkinter.CTkEntry(janela, placeholder_text="E-mail")
-senha = customtkinter.CTkEntry(janela, placeholder_text="Senha", show="*")
-botão = customtkinter.CTkButton(janela, text="Login", command=clique)
+senha = customtkinter.CTkEntry(janela, placeholder_text="Senha", show='*')
+botao_mostrar_senha = customtkinter.CTkButton(janela, text="MOSTRAR A SENHA", command=mostar_senha)
+botao_login = customtkinter.CTkButton(janela, text="Login", command=clique)
 
-texto.pack(padx=10 , pady=10)
-email.pack(padx=10 , pady=10)
-senha.pack(padx=10 , pady=10)
-botão.pack(padx=10 , pady=30)
+texto.pack(padx=10, pady=10)
+email.pack(padx=10, pady=10)
+senha.pack(padx=10, pady=10)
+botao_login.pack(padx=60, pady=10)
+botao_mostrar_senha.pack(padx=10, pady=10)
+
 
 janela.mainloop()
+ 
